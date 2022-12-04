eb0c108228cd67e157e143ab0220482ada0c08bb
AmineBouzidi <amineboozidi@gmail.com>
2022-12-04 18:43:22 +0100

    First Commit

---
+++ src/resources/textures.js
@@ -0,0 +1,47 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = '../src/jsm/terpSolutions.png';
+billboardTextures.bagHolderBetsTexture = '../src/jsm/Bagholdersbetsbillboard.png';
+billboardTextures.homeSweetHomeTexture = '../src/jsm/home-sweet-home-portrait.png';
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.twitter = '../src/jsm/twitter.png';
+boxTexture.LinkedIn = '../src/jsm/linkedInLogo.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.globe = '../src/jsm/thunder.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+boxTexture.writing = '../src/jsm/writing.png';
+
+//material textures
+let stoneTexture = '../src/jsm/stone.png';
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+//text
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';
+inputText.activities = '../src/jsm/activities_text.png';
+inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';
+inputText.homeSweetHomeText = '../src/jsm/home-sweet-home-text.png';
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+//URLs
+let URL = {};
+URL.terpsolutions = 'https://www.linkedin.com/in/amine-bouzidi-337a561ba/';
+URL.bagholderBets = '';
+URL.homeSweetHomeURL = 'https://www.linkedin.com/in/amine-bouzidi-337a561ba/';
+URL.gitHub = 'https://www.linkedin.com/in/amine-bouzidi-337a561ba/';
+URL.githubBagholder = 'https://www.instagram.com/amine_bouzidiii/';
+URL.githubHomeSweetHome = 'https://www.facebook.com/amine.bouzidi.16718';
+URL.devTo = '';
+
+export { billboardTextures, boxTexture, inputText, URL, stoneTexture, woodTexture };
